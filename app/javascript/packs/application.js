a72a0501aef609b0aef687f3beef86e963c1cd7f
Corrigan Baker <corriganbaker@gmail.com>
2022-10-05 18:05:55 -0400

    replaced js files

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,5 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
