0a7bb39929a893a6ec50265f7b7be21f32268ff1
Corrigan Baker <corriganbaker@gmail.com>
2022-10-05 18:03:14 -0400

    removed unnecessary js files

--- app/javascript/packs/application.js
+++
@@ -1,5 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
